3e5ddd402885c1689c556d9265003b1b0fc11956
Nurhax <iqbalnur2009@gmail.com>
2024-05-22 20:15:46 +0700

    Sisa class yang ketinggalan beserta UI Files Placeholder

---
+++ Cerdik-Desktop/src/cerdik/desktop/Pasien.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cerdik.desktop;
+
+/**
+ *
+ * @author KnightlyTech
+ */
+public class Pasien extends Account implements SignUp,Displays{
+    private int Usia;
+    
+    
+    @Override
+    public void Login() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void signUp() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void DisplayInfo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+    
+}
---
+++ Cerdik-Desktop/src/cerdik/desktop/TenagaKesehatan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cerdik.desktop;
+
+/**
+ *
+ * @author KnightlyTech
+ */
+public class TenagaKesehatan extends Account implements SignUp,Displays {
+    private String Phone_Nakes;
+    
+    
+    @Override
+    public void Login() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void signUp() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void DisplayInfo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+    
+}
